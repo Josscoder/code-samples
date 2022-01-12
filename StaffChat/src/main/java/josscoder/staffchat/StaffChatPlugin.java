7e1982c4e000cdbd76cf1181f7fd82f74070f291
Josscoder <josscoder@hotmail.com>
2022-01-12 14:14:12 -0500

    feat: staffchat module setup

---
+++ StaffChat/src/main/java/josscoder/staffchat/StaffChatPlugin.java
@@ -0,0 +1,16 @@
+package josscoder.staffchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaffChatPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
